bab04293544b38e6ef783939464bd35340322ce2
Sadikul Islam <sadikulislam086@gmail.com>
2025-05-29 17:34:54 +0600

    added DELETE endpoint

--- main.py
+++ main.py
@@ -149,4 +149,17 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
      save_data(data)
 
      return JSONResponse(status_code=200, content={'message': 'Patient updated successfully'})
+
      
+@app.delete("/delete/{patient_id}")
+def delete_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': 'Patient deleted successfully'})
